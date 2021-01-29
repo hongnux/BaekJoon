894a5e02fcdd2d918b043e8e996fc6c9022a97fc
hongnux <hom2955@gmail.com>
2021-01-29 18:41:26 +0900

    시험 성적

---
+++ 브론즈/9498.swift
@@ -0,0 +1,19 @@
+// 9498 - 시험성적
+import Foundation
+
+let input = Int(readLine()!)!
+
+switch input/10 {
+case 9,10 :
+    print("A")
+case 8:
+    print("B")
+case 7:
+    print("C")
+case 6:
+    print("D")
+default:
+    print("F")
+}
+    
+    
