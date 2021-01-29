1ec9ff234bade7ebcc2c93f096390f12b9dadd14
hongnux <hom2955@gmail.com>
2021-01-29 10:21:54 +0900

    그대로출력하기

---
+++ 브론즈/11718.swift
@@ -0,0 +1,11 @@
+// 11718 - 문자열 그대로 출력하기
+
+for _ in 1..<101 {
+    guard let scan = readLine() else { break }
+    print(scan)
+}
+
+// while 문을 이용한 옵셔널 바인딩
+//while let scan = readLine() {
+//    print(scan)
+//}
