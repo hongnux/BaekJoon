e02e3d84576202e6b1dab0dab8ad946274c06b7e
hongnux <hom2955@gmail.com>
2021-12-19 16:15:41 +0900

    2941

--- 실버/2941.swift
+++ 실버/2941.swift
@@ -1,6 +1,7 @@
-// 2941 - 크로아티아 알파벳
- /*
-        크로아티아 알파벳 리스트
+/*
+ 문제
+ 예전에는 운영체제에서 크로아티아 알파벳을 입력할 수가 없었다. 따라서, 다음과 같이 크로아티아 알파벳을 변경해서 입력했다.
+ 크로아티아 알파벳 리스트
         { c= }
         { c- }
         { dz= }
@@ -9,14 +10,24 @@
         { nj }
         { s= }
         { z= }
+ 예를 들어, ljes=njak은 크로아티아 알파벳 6개(lj, e, š, nj, a, k)로 이루어져 있다. 단어가 주어졌을 때, 몇 개의 크로아티아 알파벳으로 이루어져 있는지 출력한다.
+
+ dž는 무조건 하나의 알파벳으로 쓰이고, d와 ž가 분리된 것으로 보지 않는다. lj와 nj도 마찬가지이다. 위 목록에 없는 알파벳은 한 글자씩 센다.
+ 
+ 입력
+ 첫째 줄에 최대 100글자의 단어가 주어진다. 알파벳 소문자와 '-', '='로만 이루어져 있다.
+
+ 단어는 크로아티아 알파벳으로 이루어져 있다. 문제 설명의 표에 나와있는 알파벳은 변경된 형태로 입력된다.
+ 
+ 출력
+ 입력으로 주어진 단어가 몇 개의 크로아티아 알파벳으로 이루어져 있는지 출력한다.
+ ljes=njak
  */
 import Foundation
-let croatiaList: Array<String> = ["c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="]
-
 var input = readLine()!
+let croatiStringList = ["c=","c-","dz=","d-","lj","nj","s=","z="]
 
-for i in 0..<croatiaList.count {
-    input = input.replacingOccurrences(of: croatiaList[i], with: "*")
+for i in croatiStringList {
+    input = input.replacingOccurrences(of: i, with: "@")
 }
-
 print(input.count)
